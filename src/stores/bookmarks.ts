bce51568213072f49342cf848b033951ea58bfcc
Daniel Kauffmann <danielvenna2@gmail.com>
2023-10-04 22:40:44 -0300

    feat: add bookmarks

--- src/stores/bookmarks.ts
+++ src/stores/bookmarks.ts
@@ -1,14 +1,16 @@
-import { getDatabase, push, ref as dbRef, update } from 'firebase/database'
+import { getDatabase, push, ref as dbRef, update, onValue, get, child } from 'firebase/database'
 import { defineStore } from 'pinia'
 import { computed } from 'vue'
-import { useDatabaseList } from 'vuefire'
+import { getCurrentUser, useDatabaseList, useFirestore } from 'vuefire'
 import type { Tweet } from '@/stores/timeline'
+import { collection, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
 
 export const useBookmarksStore = defineStore(
   'bookmarks',
   () => {
-    const db = getDatabase()
-    const timelineRef = dbRef(db, 'timeline')
+    const firestore = useFirestore()
+    const realtime = getDatabase()
+    const timelineRef = dbRef(realtime, 'timeline')
     const bookmarksTimeline = useDatabaseList<Tweet>(timelineRef)
 
     const bookmarks = computed(() =>
@@ -18,15 +20,56 @@ export const useBookmarksStore = defineStore(
       )
     )
 
-    async function addBookmark(tweet: Tweet) {
+    async function getBookmarks(): Promise<Array<Tweet>> {
+      const tweets: Array<Tweet> = []
+      const user = await getCurrentUser()
 
+      const userRef = await doc(firestore, 'bookmarks', user!.uid)
+      const userDoc = await getDoc(userRef)
+
+      if (userDoc.exists()) {
+        const tweetsIds = userDoc.data().tweets
+
+        for (const tweetId of tweetsIds) {
+          const databaseRef = dbRef(getDatabase());
+          const data = await get(child(databaseRef, 'timeline/' + tweetId))
+
+          tweets.push(data.val() as Tweet)
+        }
+      }
+
+      return tweets
+    }
+
+    async function addBookmark(tweet: Tweet) {
+      const user = await getCurrentUser()
+
+      const userRef = await doc(firestore, 'bookmarks', user!.uid)
+      const userDoc = await getDoc(userRef)
+
+      if (userDoc.exists()) {
+        const data = userDoc.data()
+
+        await updateDoc(userRef, {
+          tweets: [
+            ...data.tweets,
+            tweet.id
+          ]
+        })
+      } else {
+        await setDoc(userRef, {
+          tweets: [
+            tweet.id,
+          ]
+        })
+      }
     }
 
     async function removeBookmark(tweet: Tweet) {
 
     }
 
-    return { addBookmark, removeBookmark, bookmarks }
+    return { addBookmark, removeBookmark, getBookmarks, bookmarks }
   },
   { persist: true }
 )
--- src/views/BookmarksView.vue
+++ src/views/BookmarksView.vue
@@ -3,15 +3,23 @@ import AppBar from '@/components/app/AppBar.vue'
 import { RecycleScroller } from 'vue-virtual-scroller'
 import { useBookmarksStore } from '@/stores/bookmarks'
 import TweetPost from '@/components/home/TweetPost.vue'
+import { onBeforeMount, ref } from 'vue'
+import type { Tweet } from '@/stores/timeline'
 
 const bookmarksStore = useBookmarksStore()
+
+const bookmarks = ref<Array<Tweet>>([])
+
+onBeforeMount(async () => {
+  bookmarks.value = await bookmarksStore.getBookmarks()
+})
 </script>
 
 <template>
   <AppBar>
     <h1 class='title'>Bookmarks</h1>
 
-    <RecycleScroller v-slot='{ item }' :item-size='32' :items='bookmarksStore.bookmarks' key-field='id'>
+    <RecycleScroller v-slot='{ item }' :item-size='32' :items='bookmarks' key-field='id'>
       <TweetPost :tweet='item' />
     </RecycleScroller>
   </AppBar>
